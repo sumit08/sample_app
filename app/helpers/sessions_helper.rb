9b732bba19b74e7bf243f6f40ba98a1f5e779418
Sumit Agarwal <agr.sumit@gmail.com>
2015-03-12 18:31:06 +0530

    Add user microposts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -34,4 +34,15 @@ module SessionsHelper
   def store_location
     session[:return_to] = request.url
   end
+
+  def current_user?(user)
+    user == current_user
+  end
+
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
+  end
 end
